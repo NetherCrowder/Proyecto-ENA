651ca67c917d2f57d88136c6ae9922d9099dcaec
NetherCrowder <luisaaronusbmed@gmail.com>
2024-09-26 11:43:51 -0500

    Actu 09-24B
    Cambio de gráficos de movimiento en el  mapa. Este cambio es el último cambio de la Alfa del proyecto realizado en Flask y se comenzará con el trabajo de la fase Beta en Django.

--- static/js/mapa.js
+++ static/js/mapa.js
@@ -14,19 +14,23 @@ document.addEventListener("DOMContentLoaded", function() {
     // Inicialización del mapa
     var map = L.map('map').setView([6.2442, -75.5812], 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-    var polyline = L.polyline([], {color: 'red'}).addTo(map);
+    var markers = [];
 
     function updateMap(data) {
         const gpsData = data.map(item => [item.gps.latitud, item.gps.longitud]);
         
-        // Agregar el nuevo punto al polígono
+        // Agregar el nuevo punto como un marcador circular
         if (gpsData.length > 0) {
             const lastPoint = gpsData[gpsData.length - 1];
-            polyline.addLatLng(lastPoint);
+            const marker = L.circleMarker(lastPoint, {color: 'red', radius: 5}).addTo(map);
+            markers.push(marker);
         }
 
-        // Ajustar el mapa a los límites de la polilínea
-        map.fitBounds(polyline.getBounds());
+        // Ajustar el mapa a los límites de los marcadores
+        if (markers.length > 0) {
+            const group = new L.featureGroup(markers);
+            map.fitBounds(group.getBounds());
+        }
     }
 
     fetchGetGPS();
